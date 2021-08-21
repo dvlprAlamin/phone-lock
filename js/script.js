8de079a34cad8f611772acfde1fef207b826dbf0
Alamin Howlader <alaminhowladerofficial@gmail.com>
2021-08-21 10:33:23 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ const waitingTime = document.getElementById('waiting-time');
 const deleteBtn = document.getElementById('delete-btn');
 const countItems = document.getElementsByClassName('count-item');
 
-document.getElementById('keys').addEventListener('click', function (e) {
+
+
+const pinCheck = (e) => {
     if (e.target.classList.value == 'key') {
         keyCount.value = keyCount.value + e.target.innerText;
     }
     for (let i = 0; i < keyCount.value.length; i++) {
-        countItems[i].style.background = '#9eabbc'
+        countItems[i].style.background = '#9eabbc';
     }
     if (keyCount.value.length == 4) {
         if (keyCount.value == '5555') {
@@ -22,54 +24,54 @@ document.getElementById('keys').addEventListener('click', function (e) {
             phone.classList.add('shake-phone');
             setTimeout(() => {
                 phone.classList.remove('shake-phone');
-                clearInterval(interval)
+                clearInterval(interval);
             }, 1000);
             const interval = setInterval(() => {
                 for (let i = 0; i < 4; i++) {
-                    countItems[i].style.background = 'transparent'
+                    countItems[i].style.background = 'transparent';
                 }
             }, 900);
             tryLeft.parentNode.classList.remove('d-none');
-            tryLeft.innerText--
+            tryLeft.innerText--;
             keyCount.value = '';
             callAction();
         }
     }
     if (keyCount.value.length > 0) {
-        deleteBtn.style.visibility = 'visible'
+        deleteBtn.style.visibility = 'visible';
     } else {
-        deleteBtn.style.visibility = 'hidden'
+        deleteBtn.style.visibility = 'hidden';
     }
-})
+}
+document.getElementById('keys').addEventListener('click', pinCheck)
 
-
-
-
-deleteBtn.addEventListener('click', function () {
+const deleteBtnHandler = () => {
     keyCount.value = keyCount.value.slice(0, -1);
     for (let i = keyCount.value.length; i < 4; i++) {
-        countItems[i].style.background = 'transparent'
+        countItems[i].style.background = 'transparent';
     }
-})
+}
+
+deleteBtn.addEventListener('click', deleteBtnHandler)
 
 const callAction = () => {
     if (tryLeft.innerText === '0') {
-        const keys = document.getElementsByClassName('key')
+        const keys = document.getElementsByClassName('key');
         for (const key of keys) {
-            key.setAttribute('disabled', true)
+            key.setAttribute('disabled', true);
         }
-        message.classList.add('d-none')
-        waitingTime.parentNode.classList.remove('d-none')
+        message.classList.add('d-none');
+        waitingTime.parentNode.classList.remove('d-none');
         setTimeout(() => {
             for (const key of keys) {
-                key.removeAttribute('disabled')
+                key.removeAttribute('disabled');
             }
             tryLeft.innerText = 5;
             clearInterval(interval);
             waitingTime.innerText = 10;
             waitingTime.parentNode.classList.add('d-none');
             message.classList.remove('d-none');
-            tryLeft.parentNode.classList.add('d-none')
+            tryLeft.parentNode.classList.add('d-none');
         }, 10000);
         const interval = setInterval(() => waitingTime.innerText--, 1000);
     }
